c9211b7b76d82be022fca13d3442d1ec99d19c77
Atanas Atanasov <nikkonix@gmail.com>
2025-11-04 11:53:51 +0200

    Fix typos

--- task_2/terraform/main.tf
+++ task_2/terraform/main.tf
@@ -100,7 +100,6 @@ resource "aws_db_instance" "mydb" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t2.micro"
-  key_name             = var.key_name
   name                 = var.db_name
   username             = var.db_user
   password             = var.db_password
