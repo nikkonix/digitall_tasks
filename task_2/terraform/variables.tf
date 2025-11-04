34e344e59480b8cdee69531895233f4471e87223
Atanas Atanasov <nikkonix@gmail.com>
2025-11-04 14:03:06 +0200

    Fix typos

--- task_2/terraform/variables.tf
+++ task_2/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" { default = "eu-north-1"}
 variable "vpc_cidr" { default = "10.0.0.0/16" }
 variable "web_instance_count" { default = 2 }
-variable "instance_type" { default = "t2.micro" }
+variable "instance_type" { default = "t3.micro" }
 variable "key_name" { default = "test-key" }
 variable "db_name" { default = "taks2_db" }
 variable "db_user" { default = "task2_dbuser" }
