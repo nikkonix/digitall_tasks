48949b4473b59e2781668e2511923eb078477dbb
Atanas Atanasov <nikkonix@gmail.com>
2025-11-04 14:42:00 +0200

    Fix typos

--- task_2/terraform/main.tf
+++ task_2/terraform/main.tf
@@ -17,6 +17,40 @@ resource "aws_subnet" "public_b" {
   availability_zone = "${var.aws_region}b"
 }
 
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-gateway"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Route Table Association (link subnet to route table)
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+
 # Security Group
 resource "aws_security_group" "web_sg" {
   name        = "web_sg"
