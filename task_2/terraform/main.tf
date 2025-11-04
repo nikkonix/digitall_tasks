73cf1c3ee5b00e954028137a1f8f683f05d84974
Atanas Atanasov <nikkonix@gmail.com>
2025-11-04 13:11:00 +0200

    Fix typos

--- task_2/terraform/main.tf
+++ task_2/terraform/main.tf
@@ -71,7 +71,7 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
   key_name      = var.key_name
   subnet_id     = aws_subnet.public.id
-  security_groups = [aws_security_group.web_sg.name]
+  security_groups = [aws_security_group.web_sg.id]
   tags = {
     Name = "WebServer-${count.index}"
   }
@@ -117,8 +117,7 @@ resource "aws_instance" "db" {
   instance_type = var.instance_type
   key_name      = var.key_name
   subnet_id     = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
-
+  security_groups = [aws_security_group.db_sg.id]
   tags = {
     Name = "mysql-db"
   }
