b8a8b5ab84e48a372629eb9e4c45c3f8c42f8596
Atanas Atanasov <nikkonix@gmail.com>
2025-11-04 13:57:41 +0200

    Fix typos

--- task_2/terraform/main.tf
+++ task_2/terraform/main.tf
@@ -6,7 +6,6 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "${var.aws_region}a"
 }
 
 # Security Group
