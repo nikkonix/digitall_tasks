7d9cec5237002f41a00a0d84ae7ff419b156d482
Atanas Atanasov <nikkonix@gmail.com>
2025-11-04 12:16:17 +0200

    Fix typos

--- task_2/terraform/provider.tf
+++ task_2/terraform/provider.tf
@@ -2,5 +2,4 @@ provider "aws" {
   region  = var.aws_region
   shared_config_files      = ["$HOME/.aws/config"]
   shared_credentials_files = ["$HOME/.aws/credentials"]
-  profile                  = "default"
 }
